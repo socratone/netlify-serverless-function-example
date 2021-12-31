1183d7359cf9640b1aa0e0764ad353b87792637a
John Kim <john@gymtinc.com>
2021-12-31 22:37:49 +0900

    fix: basic serverless function 구현

---
+++ examples/basic/index.js
@@ -0,0 +1,16 @@
+const getData = async () => {
+  try {
+    const response = await fetch('/.netlify/functions/hello');
+    const text = await response.text();
+    return text;
+  } catch {
+    return '';
+  }
+};
+
+const init = async () => {
+  const data = await getData();
+  document.body.append(data);
+};
+
+init();
