9387e8bfac8bfaedeea405bc46e98fd7a70c747a
John Kim <john@gymtinc.com>
2022-01-01 13:43:12 +0900

    fix: CORS 에러를 위한 설정 추가

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,10 @@
 exports.handler = async (event, context, callback) => {
   // Promise를 return 해야 한다.
   return {
+    // CORS 에러 없이 모든 곳에서 접근 가능하게 하려면 아래처럼 headers 속성을 넣을 수 있다.
+    // headers: {
+    //   'Access-Control-Allow-Origin': '*',
+    // },
     statusCode: 200,
     body: 'hello world', // 반드시 string이어야 한다. string이 아닌 경우에는 JSON.stringify 메소드를 써야 한다.
   };
